f2c0efa119f9b5850d79eff3a8e06d5e2e1a7678
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-12-21 13:55:25 +0100

    file move btw branches

--- sort.h
+++ sort.h
@@ -18,23 +18,21 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void swap(int *a, int *b);
-void print_array(const int *array, size_t size);
-void bubble_sort(int *array, size_t size);
-void print_list(const listint_t *list);
-void insertion_sort_list(listint_t **list);
-void swap_values(listint_t *node1, listint_t *node2);
-void quick_sort(int *array, size_t size);
 void quick_sort_recursion(int array[], size_t low, size_t high);
 size_t Lomuto_partition(int array[], size_t low, size_t high);
-
-void counting_sort(int *array, size_t size);
-
-void shell_sort(int *array, size_t size);
-
-
-void heap_sort(int *array, size_t size);
+void swap_values(listint_t *node1, listint_t *node2);
 void heapify(int *array, size_t size, size_t root);
+void print_array(const int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void bubble_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+void swap(int *a, int *b);
+
+
 
 
 #endif /* End of Include Guard */
