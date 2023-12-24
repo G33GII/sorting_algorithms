26bfb7617fb22403b76e6718cffc89cb5fbcf03f
G33GII <gazasolomon@gmail.com>
2023-12-24 23:33:42 +0100

    update lumoto function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,7 +10,7 @@ void quick_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	quick_sort_lomuto_recursion(array, size, 0, size - 1);
+	lomuto_recursion(array, size, 0, size - 1);
 }
 
 /**
