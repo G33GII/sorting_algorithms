695570f011aabd431a5bd5d7da3af0a5c121ba54
G33GII <gazasolomon@gmail.com>
2023-12-24 22:11:55 +0100

    update on task 2

--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ void quick_sort_recursion(int array[], size_t low, size_t high);
 size_t Lomuto_partition(int array[], size_t low, size_t high);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_list(const listint_t *list);
