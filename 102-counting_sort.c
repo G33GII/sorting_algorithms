aca56a12e9b1d04cbfd9926e44e1a8153eaa4de4
G33GII <gazasolomon@gmail.com>
2023-12-26 23:04:26 +0100

    update on counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,50 +1,75 @@
 #include "sort.h"
+
 /**
- * counting_sort - function to sort algorithim with counting sort
+ * counting_sort - function to sort an array using counting sort algorithm
  * @array: array to be sorted
  * @size: size of array
  * Return: void
  */
 void counting_sort(int *array, size_t size)
 {
-	int max = array[0];
-	size_t a, e, f, c;
-	int *count, *sorted;
-	int b, d;
+	/* Declare variables */
+	int *counting_array, *sorted_array, max_value, index;
 
-	for (a = 1; a < size; a++)
+	/* Check if the array is valid and has more than one element */
+	if (array == NULL || size < 2)
+		return;
+	/* Allocate memory for the sorted array */
+	sorted_array = malloc(sizeof(int) * size);
+	if (sorted_array == NULL)
+		return;
+	/* Find the maximum value in the array */
+	max_value = get_max(array, size);
+	/* Allocate memory for the counting array */
+	counting_array = malloc(sizeof(int) * (max_value + 1));
+	if (counting_array == NULL)
 	{
-		if (array[a] > max)
-		{
-			max = array[a];
-		}
+		free(sorted_array);
+		return;
 	}
+	/* Initialize the counting array with zeros */
+	for (index = 0; index < (max_value + 1); index++)
+		counting_array[index] = 0;
 
-	count = (int *)malloc((max + 1) * sizeof(int));
-	for (b = 0; b <= max; b++) {
-		count[b] = 0;
+	/* Count the occurrences of each number in the array */
+	for (index = 0; index < (int)size; index++)
+		counting_array[array[index]] += 1;
+	/* Calculate cumulative counts */
+	for (index = 0; index < (max_value + 1); index++)
+		counting_array[index] += counting_array[index - 1];
+	/* Print the counting array */
+	print_array(counting_array, max_value + 1);
+	/* Build the sorted array using the counting array */
+	for (index = 0; index < (int)size; index++)
+	{
+		sorted_array[counting_array[array[index]] - 1] = array[index];
+		counting_array[array[index]] -= 1;
 	}
+	/* Copy sorted array back to the original array */
+	for (index = 0; index < (int)size; index++)
+		array[index] = sorted_array[index];
+	/* Free the dynamically allocated memory */
+	free(sorted_array);
+	free(counting_array);
+}
 
-	for (c = 0; c < size; c++) {
-		count[array[c]]++;
+/**
+ * get_max - Finds the maximum value in an array.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max_value, index;
+
+	/* Initialize max_value with the first element of the array */
+	max_value = array[0];
+	/* Iterate over the array, updating max_value with any value larger than it */
+	for (index = 1; index < size; index++)
+	{
+		if (array[index] > max_value)
+			max_value = array[index];
 	}
-
-	print_array(count, max + 1);
-
-	for (d = 1; d <= max; d++) {
-		count[d] += count[d - 1];
-	}
-
-	sorted = (int *)malloc(size * sizeof(int));
-	for (e = size - 1; e < size; e--) {
-		sorted[count[array[e]] - 1] = array[e];
-		count[array[e]]--;
-	}
-
-	for (f = 0; f < size; f++) {
-		array[f] = sorted[f];
-	}
-
-	free(count);
-	free(sorted);
+	return (max_value);
 }
--- sort.h
+++ sort.h
@@ -21,18 +21,20 @@ typedef struct listint_s
 
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **current);
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **current);
-
 size_t Lomuto_partition(size_t size, int array[], int left, int right);
 void lomuto_recursion(int array[], size_t size, int left, int right);
 listint_t *create_listint(const int *array, size_t size);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void shell_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_list(const listint_t *list);
+int get_max(int *array, int size);
 void swap(int *a, int *b);
 
 #endif /* End of Include Guard */
